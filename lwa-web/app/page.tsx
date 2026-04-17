ee9e0139c2d9f48981cfce6d6d53bc29fccf5c8b
jcamacho611 <justinrcamacho@gmail.com>
2026-04-17 06:22:07 -0400

    Wrap home route in mythic cinematic shell

--- lwa-web/app/page.tsx
+++ lwa-web/app/page.tsx
@@ -1,5 +1,13 @@
 import { ClipStudio } from "../components/clip-studio";
+import InteractiveMythicScene from "../components/landing/InteractiveMythicScene";
 
 export default function HomePage() {
-  return <ClipStudio initialSection="home" />;
+  return (
+    <div className="home-cinematic-shell relative min-h-screen overflow-hidden">
+      <InteractiveMythicScene />
+      <div className="relative z-10">
+        <ClipStudio initialSection="home" />
+      </div>
+    </div>
+  );
 }
